a8991e813fc74ea6820580eccb9ac79825f12bef
carcormir <carcormir@gmail.com>
2023-05-02 09:41:04 +0200

    color update for transitoneffect

--- components/TransitionEffect.jsx
+++ components/TransitionEffect.jsx
@@ -13,7 +13,7 @@ export default function TransitionEffect() {
       animate={{ x: "0%", width: "0%" }}
       transition={{ delay: 0.1, duration: 0.8, ease: "easeInOut" }}
       />
-      <motion.div className="fixed top-0 bottom-0 left-full h-screen w-screen z-10 bg-dark " 
+      <motion.div className="fixed top-0 bottom-0 left-full h-screen w-screen z-10 bg-blue-500 " 
       initial={{ x: "-100%", width: "100%" }}
       animate={{ x: "0%", width: "0%" }}
       transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
