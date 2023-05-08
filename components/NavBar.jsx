f169781a08e898370a6780b3390497cfaefc6c38
carcormir <carcormir@gmail.com>
2023-05-08 15:07:59 +0200

    moved social icons to main page instead of header bar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,77 +1,28 @@
 import Link from "next/link";
 import Logo from '@/components/Logo'
-import { LinkedinIcon, GithubIcon } from '@/components/Icons'
-import { useRouter } from "next/router";
+import CustomNavLink from '@/components/CustomNavLink'
 import { motion } from 'framer-motion'
 
-const GITHUB_URL = 'https://github.com/carcormir'
-const LINKEDIN_URL = 'https://www.linkedin.com/in/carcormir/'
 
-function CustomLink ({href, title, className=`${className}`}) {
-  const router = useRouter()
-  return (
-    <Link href={href} className={`${className} relative group`}>
-      {title}
-      <span className={`
-        w-[1px] inline-block bg-dark h-full
-        absolute -left-2 bottom-0 group-hover:h-full trasition-[height] ease duration-300
-        ${router.asPath === href ? 'h-full' : 'h-0'}
-        `}>
-          &nbsp;
-      </span>
-      <span className={`
-        h-[1px] inline-block bg-dark 
-        absolute left-0 -bottom-0.5 group-hover:w-full trasition-[width] ease duration-300
-        ${router.asPath === href ? 'w-full' : 'w-0'}
-      `}>
-        &nbsp;
-      </span>
-    </Link>
-
-  )
-
-}
 
 export default function NavBar () {
   return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between bg-blue-300'>
-      <nav>
-        <CustomLink href='/' title='Home' className='mr-4'/>
-        <CustomLink href='/about' title='About' className='mx-4'/>
-        <CustomLink href='/projects' title='Projects' className='ml-4'/>
-      </nav>
+    <header className='w-full px-20 py-4 font-medium flex items-center justify-between bg-light '>
       <nav className="flex items-center justify-center flex-wrap">
-        <motion.a 
-          href={GITHUB_URL} 
-          target={'_blank'}
-          className='w-6 mx-3'
-          whileHover={{
-            translateY: -5,
-          }}
-          whileTap={{
-            scale: 0.9
-          }}
-          >
-          <GithubIcon/>
-        </motion.a>
-        <motion.a 
-          href={LINKEDIN_URL} 
-          target={'_blank'}
-          className='w-6 ml-3'
-          whileHover={{
-            translateY: -5,
-          }}
-          whileTap={{
-            scale: 0.9
-          }}
-          >
-          <LinkedinIcon/> 
-        </motion.a>
+        <div className="mr-3">
+          <Logo />
+        </div>
       </nav>
+      <nav>
+        <CustomNavLink href='/' title='Home' className='mr-4  font-semibold'/>
+        <CustomNavLink href='/about' title='About' className='mx-4 font-semibold'/>
+        <CustomNavLink href='/projects' title='Projects' className='ml-4 font-semibold'/>
+      </nav>
+      {/* <div className="absolute left-[50%] top-5 translate-x-[-50%]">
+          <Logo />
+        </div> */}
 
-      <div className="absolute left-[50%] top-5 translate-x-[-50%]">
-        <Logo />
-      </div>
+      
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import NavBar from '@/components/NavBar'
+import { motion } from 'framer-motion'
 import Layout from '@/components/Layout'
 import AnimatedText from '@/components/AnimatedText'
-import profilePic from '@/public/images/profile/developer-pic-1.png'
 import TransitionEffect from '@/components/TransitionEffect'
+import { LinkedinIcon, GithubIcon } from '@/components/Icons'
+import profilePic from '@/public/images/profile/developer-pic-1.png'
+
+
+const GITHUB_URL = 'https://github.com/carcormir'
+const LINKEDIN_URL = 'https://www.linkedin.com/in/carcormir/'
 
 export default function Home() {
   return (
@@ -14,26 +19,57 @@ export default function Home() {
         <Layout className='pt-0'>
           <div className='flex items-center justify-between w-full'>
             <div className='w-1/2 flex flex-col items-center self-center'>
-              <AnimatedText text={'Hi, I am Carlos'} className='!text-6xl text-left'/>
+              <AnimatedText text={'Frontend Developer'} className='!text-6xl text-left'/>
               <p className='my-4 text-base font-medium'>
-                Aiming to help create something special and unique through technology by being passionate, 
-                creative and having a huge determination. It is never too late to make a difference. 
-                Open for new exciting opportunities as a FrontEnd Developer.
+                Hi, I am Carlos. I am very passionate about Frontend development, specially React.js. I am aiming to help create 
+                something special and unique through technology by being dedicated, 
+                creative and having a huge determination.
               </p>
-              <div className='flex items-center self-start'>
-                <Link href='/dummy.pdf' target={'_blank'} 
-                      className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
-                     border-2 border-solid border-transparent hover:bg-light hover:text-dark hover:border-dark'
-                     download={true}
-                     >
-                  Resume
-                </Link>
-                <Link href='mailto:carcormir@gmail.com' target={'_blank'}
-                    className='ml-4 bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
-                    border-2 border-solid border-transparent hover:bg-light hover:text-dark hover:border-dark'
-                >
-                  Contact
-                </Link>
+              <div className='w-full flex items-center '>
+                <div className='flex items-center self-start'>
+                  <Link href='/dummy.pdf' target={'_blank'} 
+                        className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+                      border-2 border-solid border-transparent hover:bg-light hover:text-dark hover:border-dark'
+                      download={true}
+                      >
+                    Resume
+                  </Link>
+                  <Link href='mailto:carcormir@gmail.com' target={'_blank'}
+                      className='ml-4 bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+                      border-2 border-solid border-transparent hover:bg-light hover:text-dark hover:border-dark'
+                  >
+                    Contact
+                  </Link>
+                </div>
+                <nav className="flex items-center justify-center flex-wrap">
+                  <motion.a 
+                    href={GITHUB_URL} 
+                    target={'_blank'}
+                    className='w-8 mr-3 ml-5'
+                    whileHover={{
+                      translateY: -5,
+                    }}
+                    whileTap={{
+                      scale: 0.9
+                    }}
+                    >
+                    <GithubIcon/>
+                  </motion.a>
+                  <motion.a 
+                    href={LINKEDIN_URL} 
+                    target={'_blank'}
+                    className='w-8 ml-3'
+                    whileHover={{
+                      translateY: -5,
+                    }}
+                    whileTap={{
+                      scale: 0.9
+                    }}
+                    >
+                    <LinkedinIcon/> 
+                  </motion.a>
+        
+                </nav>
               </div>
             </div>
             <div className='w-1/2'>
