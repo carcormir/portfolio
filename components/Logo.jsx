57c79a8879d81c825a86ec1934d176c5baa5436e
carcormir <carcormir@gmail.com>
2023-05-11 08:50:39 +0200

    small logo changes

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -10,8 +10,8 @@ export default function Logo () {
         className="w-22 h-12 p-4 bg-purple-500 text-light  
                     flex items-center justify-center text-l font-bold"
         whileHover={{
-          scale:1.2,
-          backgroundColor: 'rgb(195, 177, 225)',
+          scale:1.1,
+          backgroundColor: 'rgb(192, 132, 252)',
           transition: {duration: 0.3 }
                   }}
       >
