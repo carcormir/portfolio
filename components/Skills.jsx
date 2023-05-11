738f5c4fa5d5fee2d75a3f96bea21289cc1bccc4
carcormir <carcormir@gmail.com>
2023-05-11 11:02:37 +0200

    cleanup and improving skills section

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,96 +1,42 @@
-import { CssIcon, HtmlIcon, JsIcon, TypeScriptIcon, JestIcon, 
-NextJsIcon, PythonIcon, ReactIcon, TailWindIcon, GithubIcon, ViteIcon, RobotIcon } from "./Icons"
+import { useRef, useEffect } from "react"
+import { useInView, useSpring, useMotionValue } from "framer-motion"
+import { SkillsData } from '@/constants/SkillsData'
 
-export const skillsData = [
-  {
-    type: "frontend",
-    list: [
-      {
-        name: "React",
-        icon: <ReactIcon />
-      },
-      {
-        name: "JavaScript",
-        icon: <JsIcon />
-      },
-      {
-        name: "TypeScript",
-        icon: <TypeScriptIcon />
-      },
-      {
-        name: "CSS",
-        icon: <CssIcon />
-      },
-      {
-        name: "HTML",
-        icon: <HtmlIcon />
-      },
-      {
-        name: "Next.js",
-        icon: <NextJsIcon />
-      },
-      {
-        name: "Tailwind CSS",
-        icon: <TailWindIcon />
-      },
-      {
-        name: "GitHub",
-        icon: <GithubIcon />
-      },
-      {
-        name: "Jest",
-        icon: <JestIcon />
-      },
-    ]
-  },
-  {
-    type: "others",
-    list: [
-      {
-        name: "Vite",
-        icon: <ViteIcon />
-      },
-      {
-        name: "Python",
-        icon: <PythonIcon />
-      },
-      {
-        name: "Robotics",
-        icon: <RobotIcon />
-      },
-    ]
-  },
-  {
-    type: "soft-skills",
-    list: [
-      {
-        name: "soft-1",
-        icon: <div>here</div>
-      },
-      {
-        name: "soft-2",
-        icon: <div>here</div>
-      },
-      {
-        name: "soft-3",
-        icon: <div>here</div>
-      },
-      {
-        name: "soft-4",
-        icon: <div>here</div>
-      },
-      {
-        name: "soft-5",
-        icon: <div>here</div>
-      },
-    ]
-  }
-]
+export function CustomTitle({ title, className }) {
+  const ref = useRef(null)
+
+  const motionValue = useMotionValue(0)
+  const springValue = useSpring(motionValue, {
+    duration: 5000,
+  })
+  const isInVew = useInView(ref, {once: true})
+
+  useEffect(() => {
+    if (isInVew) {
+      motionValue.set(100)
+    }
+  }, [isInVew, title, motionValue])
+
+  useEffect(() => {
+    springValue.on("change", (latestValue) => {
+      ref.current.style.width = `${latestValue}%`
+    })
+  }, [springValue, title])
+
+  return (
+    <div className={`${className} relative group`}>
+      {title}
+      <span ref={ref} className="h-[1px] inline-block bg-purple-500/75 absolute left-0 -bottom-0.5">
+        &nbsp;
+      </span>
+    </div>
+  )
+}
 
 export function SkillCard({ name, icon }) {
   return (
-    <div className="w-1/6 h-auto flex flex-col items-center justify-center px-5">
-      <div className="w-24 h-24 bg-red-400 rounded-full flex items-center justify-center">
+    <div className="w-auto h-auto flex flex-col items-center justify-center px-5">
+      <div className="w-16 h-16 flex items-center justify-center">
         {icon}
       </div>
       <div className="text-lg font-medium mt-4 text-dark/75">
@@ -103,27 +49,27 @@ export function SkillCard({ name, icon }) {
 export default function Skills() { 
   return (
       <>
-        <h2 className="font-bold text-8xl mt-36 w-full text-center">Skills</h2>
+        <h2 className="font-bold text-8xl mt-32 w-full text-center">Skills</h2>
         <div className="w-full h-screen flex flex-col items-start justify-start">
-          {
-            skillsData.map((skill, index) => {
-              return (
-                <div key={index} className="w-full h-auto flex flex-col items-start justify-start mt-10">
-                  <div className="text-3xl font-bold text-dark/75">
-                    {skill.type}
-                  </div>
-                  <div className="w-full h-1/2 flex flex-wrap items-start justify-start gap-10 mt-10">
-                    {
-                      skill.list.map((item, index) => {
-                        return (<SkillCard key={index} name={item.name} icon={item.icon} />)
-                      })
-                    } 
-                  </div>
+        {
+          SkillsData.map((skill, index) => {
+            return (
+              <div key={index} className="w-full h-auto flex flex-col items-start justify-start mt-10">
+                <div className='inline-block w-full text-7xl font-bold'>
+                  <CustomTitle title={skill.type} className="text-3xl font-bold text-dark/75" />
                 </div>
-              )
-            })
-          }
-        </div>
+                <div className="w-full h-1/2 flex flex-wrap  items-center justify-end gap-5 mt-10">
+                  {
+                    skill.list.map((item, index) => {
+                      return (<SkillCard key={index} name={item.name} icon={item.icon} />)
+                    })
+                  } 
+                </div>
+              </div>
+            )
+          })
+        }
+      </div> 
       </>
   )}
 
