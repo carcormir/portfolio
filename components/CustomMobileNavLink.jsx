df8a078b416f45d0fca943b323156c83f17f45c7
carcormir <carcormir@gmail.com>
2023-05-12 12:27:16 +0200

    mobile menu initial version

---
+++ components/CustomMobileNavLink.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+import { useRouter } from "next/router";
+
+
+export default function CustomMobileNavLink ({href, title, toggle, className=`${className}`}) {
+  const router = useRouter()
+
+  const handleClick = () => {
+    toggle()
+    router.push(href)
+  }
+
+  return (
+    <button onClick={handleClick} href={href} className={`${className} relative group my-2`}>
+      {title}
+      <span className={`
+        h-[1px] inline-block bg-light 
+        absolute left-0 -bottom-0.5 group-hover:w-full trasition-[width] ease duration-300
+        ${router.asPath === href ? 'w-full' : 'w-0'}
+      `}>
+        &nbsp;
+      </span>
+    </button>
+
+  )
+
+}
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,23 +1,49 @@
 import Link from "next/link";
 import Logo from '@/components/Logo'
 import CustomNavLink from '@/components/CustomNavLink'
+import CustomMobileNavLink from '@/components/CustomMobileNavLink'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 
 
 export default function NavBar () {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
   return (
-    <header className='w-full px-20 py-4 font-medium flex items-center justify-between bg-light '>
-      <nav className="flex items-center justify-center flex-wrap">
-        <div className="mr-3">
-          <Logo />
-        </div>
-      </nav>
-      <nav>
-        <CustomNavLink href='/' title='Home' className='mr-4  font-semibold'/>
-        <CustomNavLink href='/about' title='About' className='mx-4 font-semibold'/>
-        <CustomNavLink href='/projects' title='Projects' className='ml-4 font-semibold'/>
-      </nav>
+    <header className='w-full px-20 py-4 font-medium flex items-center justify-between bg-light relative'>
+      <button onClick={handleClick} className="flex-col justify-center items-center hidden lg:flex">
+        <span className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5 '}`}></span>
+        <span className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+        <span className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5 '}`}></span>
+      </button>
+      <div className="mr-3">
+        <Logo />
+      </div>
+      <div className="w-full flex items-center justify-end lg:hidden">
+        <nav>
+          <CustomNavLink href='/' title='Home' className='mr-4  font-semibold'/>
+          <CustomNavLink href='/about' title='About' className='mx-4 font-semibold'/>
+          <CustomNavLink href='/projects' title='Projects' className='ml-4 font-semibold'/>
+        </nav>
+      </div>
+
+      {
+        isOpen && (
+          <div className="min-w-[70vw] flex flex-col items-center justify-end fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30
+          bg-dark/90 text-light/75 rounded-lg backdrop-blur-md py-32">
+            <nav className="flex flex-col items-center justify-between">
+              <CustomMobileNavLink href='/' title='Home' className='font-semibold' toggle={handleClick}/>
+              <CustomMobileNavLink href='/about' title='About' className='font-semibold' toggle={handleClick}/>
+              <CustomMobileNavLink href='/projects' title='Projects' className='font-semibold' toggle={handleClick}/>
+            </nav>
+          </div>
+        )
+      }
+      
     </header>
   )
 }
