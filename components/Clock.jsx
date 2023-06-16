eb164091a35b45c3bbefc955d18b380f659b306e
carcormir <carcormir@gmail.com>
2023-06-16 17:08:19 +0200

    updated code with real time values. In line styles

--- components/Clock.jsx
+++ components/Clock.jsx
@@ -12,26 +12,41 @@ export default function Clock() {
     return () => clearInterval(interval)
   }, [])
 
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+   // Calculate rotation degrees for clock hands
+   const hoursRotation = (hours % 12) * 30 + minutes * 0.5;
+   const minutesRotation = minutes * 6 + seconds * 0.1;
+   const secondsRotation = seconds * 6;
+ 
+   // Inline styles for clock hands
+   const hoursStyle = {
+     transform: `rotate(${hoursRotation}deg)`,
+   };
+ 
+   const minutesStyle = {
+     transform: `rotate(${minutesRotation}deg)`,
+   };
+ 
+   const secondsStyle = {
+     transform: `rotate(${secondsRotation}deg)`,
+   };
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-full my-10">
-      {time.toLocaleTimeString()}
-      <div>
-        {/* {time.getHours()}
-        {time.getMinutes()}
-        {time.getSeconds()} */}
-      </div>
-
       <article className={styles.clock}>
         <div className={styles.hoursContainer}>
-          <div className={styles.hours}></div>
+          <div className={styles.hours} style={hoursStyle}></div>
         </div>
         <div className={styles.minutesContainer}>
-          <div className={styles.minutes}></div>
+          <div className={styles.minutes} style={minutesStyle}></div>
         </div>
         <div className={styles.secondsContainer}>
-          <div className={styles.seconds}></div>
+          <div className={styles.seconds} style={secondsStyle}></div>
         </div>
       </article>
     </div>
-  )
+  );
 }
